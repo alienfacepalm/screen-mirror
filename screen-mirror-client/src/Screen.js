27e1a74c8703bfcc4d6870ddd0c61b950604d0fd
alienfacepalm <brandon@alienfacepalm.com>
2017-04-14 14:32:43 -0700

    added swipe improvements

--- screen-mirror-client/src/Screen.js
+++ screen-mirror-client/src/Screen.js
@@ -27,11 +27,16 @@ class Screen extends Component {
 	componentDidMount() {
 		console.log(`Screen component mounted`);
 
+		this.screenContainer = document.getElementById('screen-container');
+		this.screenContainer.onmousedown = this.screenContainerDown.bind(this);
+
 		this.canvas = document.createElement('canvas');
 		this.canvas.width = this.deviceWidth/2;
 		this.canvas.height = this.deviceHeight/2;
-		this.canvas.style = 'border: 1px solid black;';
+		this.canvas.style = 'margin: 50px; border: 1px solid black; cursor: pointer;';
 
+		this.canvas.onmouseover = this.cursorOver.bind(this);
+		this.canvas.onmouseout = this.cursorOut.bind(this);
 		this.canvas.onmousedown = this.interactStart.bind(this);
 		this.canvas.onmousemove = this.interactMove.bind(this);
 		this.canvas.onmouseup = this.interactEnd.bind(this);
@@ -55,16 +60,26 @@ class Screen extends Component {
 		return {x: x, y: y};
 	}
 
+	reset(){
+		console.log(`======] Attemping Minitouch Reset [======`);
+		this.commands.push(`r`);
+		this.commands.push(`c`);
+		this.sendCommands();
+	}
+
 	sendCommands(){
+		console.log(`======] Commands [======`, this.commands);
+
 		let message = JSON.stringify({type: 'input', commands: this.commands});
 		this.websocket.send(message);
 		this.commands = [];
 	}
 
 	interactStart(event){
+		this.canvas.style.cursor = 'move';
 		this.setState({swiping: true});
-		let pos = this.calculatePosition(event);
 
+		let pos = this.calculatePosition(event);
 		this.commands.push(`d 0 ${pos.x} ${pos.y} 50`);
 		this.commands.push(`c`);
 	}
@@ -72,7 +87,6 @@ class Screen extends Component {
 	interactMove(event){
 		if(this.state.swiping){
 			let pos = this.calculatePosition(event);
-
 			this.commands.push(`m 0 ${pos.x} ${pos.y} 50`);
 			this.commands.push(`c`);
 
@@ -80,15 +94,35 @@ class Screen extends Component {
 		}
 	}
 
-	interactEnd(event){
+	interactEnd(){
+		this.canvas.style.cursor = 'pointer';
 		this.setState({swiping: false});
 
 		this.commands.push(`u 0`);
 		this.commands.push(`c`);
 
-		console.log(`======] Interaction ${this.commands} [======`);
-
 		this.sendCommands();
+	}
+
+	screenContainerDown(event){
+		console.log(`======] Container down [======`);
+		this.setState({swiping: true});
+	}
+
+	cursorOver(event){
+		console.log(`======] Cursor over canvas [======`);
+		this.canvas.style.cursor = 'pointer';
+		if(this.state.swiping){
+			this.interactStart(event);
+		}
+	}
+
+	cursorOut(event){
+		console.log(`======] Cursor left canvas [======`);
+		this.canvas.style.cursor = 'pointer';
+		if(this.state.swiping){
+			this.interactEnd();
+		}
 	}
 
 	update(data){
@@ -106,7 +140,7 @@ class Screen extends Component {
 			<div>
 				<div id="screen-container"></div>
 				<div id="nav-buttons">
-					<Icon name="home" size="3x" className="nav-icons" />
+					<Icon onClick={this.reset.bind(this)} className="nav-icons" name="refresh" size="3x" />
 				</div>
 			</div>
 		);
